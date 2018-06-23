f195c5d813b1ad1ca7582e2a066c4708c74f5289
dreamcfl <dreamcfl@163.com>
2018-06-23 14:55:38 +0800

    footer11

--- source/index.js
+++ source/index.js
@@ -1,1 +1,32 @@
-let a=10;
+$(function(){
+	$("#header_left").hover(function(){
+		$("#header_left_box").show();
+	},function(){
+		$("#header_left_box").hide();
+	})
+	$("#header_list1").hover(function(){
+		$("#header_1").show();
+	},function(){
+		$("#header_1").hide();
+	})
+	$("#header_list2").hover(function(){
+		$("#header_2").show();
+	},function(){
+		$("#header_2").hide();
+	})
+	$("#header_list3").hover(function(){
+		$("#header_3").show();
+	},function(){
+		$("#header_3").hide();
+	})
+	$("#header_list4").hover(function(){
+		$("#header_4").show();
+	},function(){
+		$("#header_4").hide();
+	})
+	$("#header_list5").hover(function(){
+		$("#header_5").show();
+	},function(){
+		$("#header_5").hide();
+	})
+})
